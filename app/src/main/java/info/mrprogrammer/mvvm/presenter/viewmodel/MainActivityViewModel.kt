082b11751b22a8bbd667651cbe9c1c7445d39f7c
Krishna Kumar Eswaran <krishnakumar.eswaran@zohocorp.com>
2024-03-30 13:43:47 +0530

    state flow

--- app/src/main/java/info/mrprogrammer/mvvm/presenter/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/info/mrprogrammer/mvvm/presenter/viewmodel/MainActivityViewModel.kt
@@ -6,6 +6,7 @@ import info.mrprogrammer.mvvm.data.UseCaseImp
 import info.mrprogrammer.mvvm.data.model.ResultModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -13,10 +14,10 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(private val useCaseImp: UseCaseImp):ViewModel() {
 
     private val _resultModelList= MutableStateFlow<List<ResultModel>>(listOf<ResultModel>())
-    val resultModelList: StateFlow<List<ResultModel>> = _resultModelList
+    val resultModelList: StateFlow<List<ResultModel>> = _resultModelList.asStateFlow()
 
     private val _progressState = MutableStateFlow<Boolean>(false)
-    val progressState: StateFlow<Boolean> = _progressState
+    val progressState: StateFlow<Boolean> = _progressState.asStateFlow()
 
     fun fetchData() {
         if (useCaseImp.isInterNetConnected()) {
